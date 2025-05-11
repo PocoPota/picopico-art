e54ba9be3c6db9c9b3705668f741b3bec39bc0cb
pocopota <pocopota.program@gmail.com>
2025-05-12 02:35:36 +0900

    Buttonコンポーネントの作成

---
+++ src/app/components/Button.tsx
@@ -0,0 +1,18 @@
+import clsx from "clsx";
+import styles from "./Button.module.scss";
+
+type ButtonProps = {
+  label: string;
+  size?: "small" | "medium" | "large";
+  onClick?: () => void;
+  className?: string;
+};
+
+export default function Button({
+  label,
+  size = "medium",
+  className,
+}: ButtonProps) {
+  const classNames = clsx(styles.button, styles[size], className);
+  return <button className={classNames}>{label}</button>;
+}
---
+++ src/app/tmp/page.tsx
@@ -0,0 +1,10 @@
+import Button from "../components/Button";
+
+export default function Tmp() {
+  return (
+    <Button
+      label="ボタン"
+      size="medium"
+    />
+  );
+}
