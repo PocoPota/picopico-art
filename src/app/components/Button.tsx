7ddd43b9d9582f33c8925ad4ea5a6c526b29acf8
pocopota <pocopota.program@gmail.com>
2025-05-12 11:42:02 +0900

    color追加

--- src/app/components/Button.tsx
+++ src/app/components/Button.tsx
@@ -4,6 +4,7 @@ import styles from "./Button.module.scss";
 type ButtonProps = {
   label: string;
   size?: "small" | "medium" | "large";
+  color?: "primary"
   onClick?: () => void;
   className?: string;
 };
@@ -11,9 +12,10 @@ type ButtonProps = {
 export default function Button({
   label,
   size = "medium",
+  color = "primary",
   className,
   ...props
 }: ButtonProps) {
-  const classNames = clsx(styles.button, styles[size], className);
+  const classNames = clsx(styles.button, styles[size], styles[color], className);
   return <button className={classNames} {...props}>{label}</button>;
 }
