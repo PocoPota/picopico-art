cc42e6fed474095197442056b765d2f7cfc7841d
pocopota <pocopota.program@gmail.com>
2025-05-14 13:51:33 +0900

    Headerアカウント表示変更

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,9 +1,15 @@
+'use client'
+
 import Image from "next/image";
 import Link from "next/link";
 import styles from "./Header.module.scss";
 import Button from "./Button";
 
+import { useAuth } from "../context/AuthContext";
+
 export default function Header() {
+  const {user, loading} = useAuth();
+
   return (
     <header className={styles.header}>
       <div className={styles.header_inner}>
@@ -26,11 +32,16 @@ export default function Header() {
           ぴこぴこアート
         </Link>
         <div className={styles.account}>
-          <Button
-            label="ログイン"
-            size="small"
-            href="/login"
-          />
+          {user ?(
+            <div>Hello {user.email}!</div>
+          ):(
+            <Button
+              label="ログイン"
+              size="small"
+              href="/login"
+            />
+          )
+        }
         </div>
       </div>
     </header>
