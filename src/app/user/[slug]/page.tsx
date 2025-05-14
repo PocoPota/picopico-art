c6728b1a3d6b860a2b944e90ee2e8dd82dac0aab
pocopota <pocopota.program@gmail.com>
2025-05-14 19:47:24 +0900

    個人プロフィール部分

--- src/app/user/[slug]/page.tsx
+++ src/app/user/[slug]/page.tsx
@@ -1,10 +1,80 @@
+"use client";
+
+import styles from "./page.module.scss";
+import Avatar from "boring-avatars";
+
+import React, { useEffect, useState } from "react";
+import { useAuth } from "@/app/context/AuthContext";
+import { collection, query, where, getDocs } from "firebase/firestore";
+import { db } from "../../lib/firebase";
+import Button from "@/app/components/Button";
+
 type PageProps = {
   params: Promise<{ slug: string }>;
 };
 
-export default async function OnePost({ params }: PageProps) {
-  const { slug } = await params;
-  return(
-    <div>{slug}</div>
+export default function User({ params }: PageProps) {
+  const { user, loading } = useAuth();
+  const unwrappedParams = React.use(params); // 👈 unwrap Promise
+  const { slug } = unwrappedParams;
+  const [userDoc, setUserDoc] = useState<any>(null);
+  const [fetching, setFetching] = useState(true);
+
+  // データの取得
+  useEffect(() => {
+    const fetchUserData = async () => {
+      try {
+        const usersRef = collection(db, "users");
+        const q = query(usersRef, where("uid", "==", slug));
+        const querySnapshot = await getDocs(q);
+        if (!querySnapshot.empty) {
+          const docData = querySnapshot.docs[0].data();
+          setUserDoc(docData);
+        } else {
+          console.warn("ユーザー情報がFirestoreに存在しません");
+        }
+      } catch (e) {
+        console.error(e);
+      } finally {
+        setFetching(false);
+      }
+    };
+    if (!loading) {
+      fetchUserData();
+    }
+  }, [user, loading]);
+
+  if (loading || fetching) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div className={styles.user}>
+      <div className={styles.profile}>
+        <div className={styles.p_user}>
+          <div className={styles.p_left}>
+            <Avatar
+              name={slug}
+              variant="beam"
+              size={70}
+            />
+          </div>
+          <div className={styles.p_right}>
+            <div className={styles.username}>{userDoc.userName}</div>
+            <div className={styles.comment}>自己紹介がここに入ります。</div>
+          </div>
+        </div>
+        {user && (
+          <div className={styles.edit}>
+            <Button
+              label="Edit Profile"
+              size="small"
+              href="/settings"
+            />
+          </div>
+        )}
+      </div>
+      <div className={styles.gallary}></div>
+    </div>
   );
 }
