9d5e8a60de4166b3ca56de186c06c978c7401151
pocopota <pocopota.program@gmail.com>
2025-05-14 13:23:10 +0900

    ユーザー名も同時に登録

--- src/app/components/SignUpForm.tsx
+++ src/app/components/SignUpForm.tsx
@@ -2,13 +2,18 @@
 "use client"; // App Router を使用している場合、クライアントコンポーネントであることを示す
 
 import React, { useState } from "react";
-import { createUserWithEmailAndPassword } from "firebase/auth";
-import { auth } from "../lib/firebase"; // ステップ4で初期化したauthオブジェクトをインポート
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import { auth, db } from "../lib/firebase"; // ステップ4で初期化したauthオブジェクトをインポート
+import { addDoc, collection } from "firebase/firestore";
+import { useRouter } from "next/navigation";
 
 const SignUpForm: React.FC = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [userName, setUserName] = useState("");
   const [error, setError] = useState<string | null>(null);
+
+  const router = useRouter();
 
   const handleSignUp = async (e: React.FormEvent) => {
     e.preventDefault(); // フォームのデフォルト送信を防ぐ
@@ -16,10 +21,19 @@ const SignUpForm: React.FC = () => {
 
     try {
       // Firebase Authentication の新規登録関数を呼び出す
-      await createUserWithEmailAndPassword(auth, email, password);
+      await createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential)=>{
+        const user = userCredential.user;
+        // DBにユーザーネームを登録
+        const docRef = addDoc(collection(db, "users"),{
+          uid: user.uid,
+          userName: userName
+        });
+      });
       // 成功した場合の処理（例: ホームページにリダイレクトなど）
       console.log("新規登録成功！");
       // useRouter などを使ってリダイレクト処理をここに記述
+      router.push("/");
     } catch (error: any) {
       // エラーが発生した場合の処理
       console.error("新規登録エラー:", error);
@@ -48,6 +62,16 @@ const SignUpForm: React.FC = () => {
             id="password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label>ユーザー名</label>
+          <input
+            type="text"
+            id="userName"
+            value={userName}
+            onChange={(e)=>setUserName(e.target.value)}
             required
           />
         </div>
