861b07364b7aae45612305796c77ba7456ab8184
pocopota <pocopota.program@gmail.com>
2025-05-21 01:22:08 +0900

    fix suspense bug

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,23 +1,13 @@
 "use client";
 
 import styles from "./page.module.scss";
+import CreateAccount from "../components/CreateAccount";
 import { Suspense } from "react";
-import SignUpForm from "../components/SignUpForm";
-import MailAuth from "../components/MailAuth";
-
-import { useSearchParams } from "next/navigation";
 
 export default function Login() {
-  const searchParams = useSearchParams();
-  const code = searchParams.get("code");
-
-  if (code) {
-    return (
-      <Suspense>
-        <SignUpForm code={code} />
-      </Suspense>
-    );
-  } else {
-    return <MailAuth />;
-  }
+  return (
+    <Suspense>
+      <CreateAccount />
+    </Suspense>
+  );
 }
